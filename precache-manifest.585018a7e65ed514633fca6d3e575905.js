8549fe3a3f596d78abc3308b29e670fe45635377
estev97 <estev97@gmail.com>
2019-09-20 22:47:49 -0600

    Final Project compilado

---
+++ precache-manifest.585018a7e65ed514633fca6d3e575905.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f242238c1ac5dd21aace",
+    "url": "/js/chunk-vendors.c161960c.js"
+  },
+  {
+    "revision": "b0a5a42695cf94a6f741",
+    "url": "/js/app.90b5cb21.js"
+  },
+  {
+    "revision": "dc03e88ce25e04f3413301078158aa7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9762bb26ed73a898b5031703bdf6f91a",
+    "url": "/img/clavala.9762bb26.svg"
+  },
+  {
+    "revision": "d1fb5bbc4e5620643e99f493dcf0fdd0",
+    "url": "/img/21501188.d1fb5bbc.jpg"
+  },
+  {
+    "revision": "bfa864de4dad0a4c56661377e03b7d2b",
+    "url": "/img/2147878.bfa864de.jpg"
+  },
+  {
+    "revision": "f242238c1ac5dd21aace",
+    "url": "/css/chunk-vendors.87cf3782.css"
+  },
+  {
+    "revision": "b0a5a42695cf94a6f741",
+    "url": "/css/app.d07e4c55.css"
+  }
+];
